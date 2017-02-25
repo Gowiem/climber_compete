d17ceace5de57a084bd590bc9e43833b143e1a3e
Matt Gowie <gowie.matt@gmail.com>
2017-02-25 16:34:38 +0700

    Adds names to User + seeds.rb

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+gowie = User.create(first_name: 'Matt',
+                    last_name: 'Gowie',
+                    email: 'gowie.matt@gmail.com',
+                    password: 'supersecret',
+                    password_confirmation: 'supersecret')
+
+gian = User.create(first_name: 'Chris',
+                   last_name: 'Gian',
+                   email: 'christopher.gian@gmail.com',
+                   password: 'supersecret',
+                   password_confirmation: 'supersecret')
+
+comp = Competition.create(name: 'Starter Comp',
+                          start_date: DateTime.new(2017),
+                          end_date: DateTime.new(2017, 4))
+
+gowie.user_comps.create(competition: comp)
+gian.user_comps.create(competition: comp)
